3418f62c76e51819b9b7637be56ec2224c266400
dsanders <dsanders@localhost.localdomain>
2019-02-06 00:37:51 -0500

    added app.js and fixed demo.js

--- demo_server.js
+++ demo_server.js
@@ -1,1 +1,11 @@
-// hello world
+
+const http = require('http');
+const port = 3000;
+const ip = '192.168.50.89';
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('<h2>Hello World</h2>');
+}).listen(port, ip);
+
+console.log(`server is running on ${ip}:${port}`);
