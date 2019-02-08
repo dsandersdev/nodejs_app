07871665b312a3d5508e75353b7a58a7ce83a88c
Duane <dsanders.dev@gmail.com>
2019-02-08 17:13:31 -0500

    set up express in app.js.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("It works!");
+var express = require('express');
+var app = express();
+app.set('port', 3000);
+var onlog = function() {
+	var port = server.address().port;
+	console.log("magic happens on port " + port);
+}
+var server = app.listen(app.get('port'), onlog);
