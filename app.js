ee0eb380aeac4177d333abb397b220ba1cdb9ecb
Duane <dsanders.dev@gmail.com>
2019-02-07 23:14:07 -0500

    added new files for learning nodejs (centos system)

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('helloworld');
+require('./instanthello');
+var goodbye = require('./talk/goodbye');
+var talk = require('./talk');
+var question = require ('./talk/question');
+
+talk.intro();
+talk.hello('Duane');
+
+var answer = question.ask("What is the meaning of life?");
+console.log(answer);
+
+goodbye();
