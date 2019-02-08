28e28358af3779b9fb464d2e03a998d907418c98
Duane <dsanders.dev@gmail.com>
2019-02-08 17:53:10 -0500

    added res and req code

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+app.get('/', function(req, res) {
+	console.log('GET the homepage');
+	res
+	.status(200)
+	.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.set('port', 3000);
+
+app.get('/json', function(req, res) {
+	console.log('GET the json');
+	res
+	.status(200)
+	.json({"jsonData" : true});
+});
+
+app.get('/file', function(req, res) {
+	console.log('GET the file');
+	res
+	.status(200)
+	.sendFile(path.join(__dirname, 'app.js'));
+});
+
 var onlog = function() {
 	var port = server.address().port;
 	console.log("magic happens on port " + port);
