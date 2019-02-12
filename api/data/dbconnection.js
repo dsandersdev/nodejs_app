2186c0aef5c115b46032a768c26eb4daa7e01ee9
Duane <dsanders.dev@gmail.com>
2019-02-11 23:32:17 -0500

    Started tutorial about connecting to mongodb inside node app.

---
+++ api/data/dbconnection.js
@@ -0,0 +1,25 @@
+var MongoClient = require("mongodb").MongoClient;
+var dburl = 'mongodb://localhost:27017/meanhotel';
+var _conn = null;
+
+var open = function() {
+	MongoClient.connect(dburl, function(err, db) {
+		if ( err ) {
+			console.log("DB connection failed");
+			return;
+		}
+		_conn = db;
+		console.log("DB connection open", db);
+	});
+	// set _connection
+	
+};
+
+var get = function() {
+	return _conn;
+};
+
+module.exports = {
+	open : open,
+	get : get
+};
